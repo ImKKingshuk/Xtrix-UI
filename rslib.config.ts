9a8bd4152a7ddb1914719f32a6a297b52b40a56d
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-11-20 09:24:56 +0530

    updated

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,26 +1,30 @@
-import { pluginReact } from '@rsbuild/plugin-react';
-import { defineConfig } from '@rslib/core';
+import { pluginReact } from "@rsbuild/plugin-react";
+import { defineConfig } from "@rslib/core";
 
 export default defineConfig({
   source: {
     entry: {
-      index: ['./src/**'],
+      index: ["./src/**"],
+      Accordion: ["./src/UI/Components/Accordion/index.ts"],
+      Avatar: ["./src/UI/Components/Avatar/index.ts"],
+      Card: ["./src/UI/Components/Card/index.ts"],
+      ThemeProvider: ["./src/UI/Providers/ThemeProvider/index.ts"],
     },
   },
   lib: [
     {
       bundle: false,
       dts: true,
-      format: 'esm',
+      format: "esm",
     },
   ],
   output: {
-    target: 'web',
+    target: "web",
   },
   plugins: [
     pluginReact({
       swcReactOptions: {
-        runtime: 'classic',
+        runtime: "classic",
       },
     }),
   ],
